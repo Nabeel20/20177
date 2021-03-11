4e9b03be2cf3f64b4901823aa7bdd54b02d1cb8e
20-9-20 <nabeelandnizam@gmail.com>
2021-03-11 08:43:02 +0200

    -_-

--- App.js
+++ App.js
@@ -5,53 +5,51 @@ import RNFS from 'react-native-fs'
 function MyComponent(){
   const [isloading, setIsLoading] = React.useState(true)
   const [errorMessage, setErrorMessage] = React.useState('error message: none')
- const [data, setData] = React.useState({stat:'none', trying:true})
+  const [data, setData] = React.useState({stat:'none', trying:true})
 
-  React.useEffect(() => {
+    React.useEffect(() => {
+        async function fetch_quiz() {
+            try {
+                return files = await readDir(RNFS.DownloadDirectoryPath);
+            } catch (error) {
+                setErrorMessage(JSON.stringify(error))
+            }
+        }
+        let output = fetch_quiz()
 
-    async function fetch_quiz() {
-      try {
-        return files = await readDir(RNFS.DownloadDirectoryPath);
-      } catch (error) {
-        setErrorMessage(JSON.stringify(error))
-      }
-    }
 
+        if (output.length > 0) {
+            setData(output);
+            setIsLoading(false)
+        } else {
+            setData({ stat: 'failed array is empty?', feeling: 'sad' });
+            setIsLoading(false)
+        }
+    }, [])
 
-
-    if(fetch_quiz().length > 0){
-      setData(fetch_quiz());
-      setIsLoading(false)
-    }else{
-      setData({stat:'failed array is empty?', feeling: 'sad'});
-      setIsLoading(false)
-    }
-  }, [])
   
   
   
  
   if (isloading) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>جاري التحميل ثوان من فضلك v38</Text>
-      <Text>{errorMessage}</Text>
-      <Text>{JSON.stringify(data, null, 2)}</Text>
-      </View>
-    )
-  }
-  
-   if (!isloading) {
-    return (
-      <View style={{ flex: 1, alignItems: 'center' }}>
-        <Text>النتيجة: v38</Text>
-        <Text>{errorMessage}</Text>
-      <Text>---------------</Text>
-      <Text>{JSON.stringify(data, null, 2)}</Text>
-      </View>
-    )
-  }
-  
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>جاري التحميل ثوان من فضلك</Text>
+                <Text>{errorMessage}</Text>
+                <Text>{JSON.stringify(data, null, 2)}</Text>
+            </View>
+        )
+    }
+
+
+        return (
+            <View style={{ flex: 1, alignItems: 'center' }}>
+                <Text>النتيجة:</Text>
+                <Text>{errorMessage}</Text>
+                <Text>---------------</Text>
+                <Text>{JSON.stringify(data, null, 2)}</Text>
+            </View>
+        )
   
   
   
