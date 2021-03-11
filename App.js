3e13614dd0f04152129f4981f7089cf17ef6c0eb
20-9-20 <nabeelandnizam@gmail.com>
2021-03-11 08:49:49 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,29 +3,10 @@ import * as React from 'react';
 import { View, StyleSheet, Text, FlatList, I18nManager } from 'react-native';
 import RNFS from 'react-native-fs'
 function MyComponent(){
-  const [isloading, setIsLoading] = React.useState(true)
   const [errorMessage, setErrorMessage] = React.useState('error message: none')
   const [data, setData] = React.useState({stat:'none', trying:true})
 
-    React.useEffect(() => {
-        async function fetch_quiz() {
-            try {
-                return files = await readDir(RNFS.DownloadDirectoryPath);
-            } catch (error) {
-                setErrorMessage(JSON.stringify(error))
-            }
-        }
-        let output = fetch_quiz()
 
-
-        if (output.length > 0) {
-            setData(output);
-            setIsLoading(false)
-        } else {
-            setData({ stat: 'failed array is empty?', feeling: 'sad' });
-            setIsLoading(false)
-        }
-    }, [])
 
   
   
@@ -45,7 +26,6 @@ function MyComponent(){
         return (
             <View style={{ flex: 1, alignItems: 'center' }}>
                 <Text>النتيجة:</Text>
-                <Text>{errorMessage}</Text>
                 <Text>---------------</Text>
                 <Text>{JSON.stringify(data, null, 2)}</Text>
             </View>
