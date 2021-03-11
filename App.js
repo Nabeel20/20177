4fc3e08f5e5618b73c513b16a8cf02b4486353a5
20-9-20 <nabeelandnizam@gmail.com>
2021-03-11 09:32:00 +0200

    !!!!

--- App.js
+++ App.js
@@ -3,16 +3,36 @@ import * as React from 'react';
 import { View, StyleSheet, Text, FlatList, I18nManager } from 'react-native';
 import RNFS from 'react-native-fs'
 function MyComponent(){
-  const [errorMessage, setErrorMessage] = React.useState('error message: none')
-  const [data, setData] = React.useState({stat:'none', trying:true})
+   const [isloading, setIsLoading] = React.useState(true)
+    const [errorMessage, setErrorMessage] = React.useState('error message: none')
+    const [data, setData] = React.useState({ stat: 'none', trying: true })
 
 
 
-  
-  
-  
- 
-  if (isloading) {
+    React.useEffect(() => {
+        async function fetch_quiz() {
+            try {
+                return files = await readDir(RNFS.DownloadDirectoryPath);
+            } catch (error) {
+                setErrorMessage(JSON.stringify(error))
+            }
+        }
+        let output = fetch_quiz()
+
+
+        if (output.length > 0) {
+            setData(output);
+            setIsLoading(false)
+        } else {
+            setData({ stat: 'failed array is empty?', feeling: 'sad' });
+            setIsLoading(false)
+        }
+    }, [])
+
+
+
+
+    if (isloading) {
         return (
             <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
                 <Text>جاري التحميل ثوان من فضلك</Text>
@@ -23,15 +43,16 @@ function MyComponent(){
     }
 
 
-        return (
-            <View style={{ flex: 1, alignItems: 'center' }}>
-                <Text>النتيجة:</Text>
-                <Text>---------------</Text>
-                <Text>{JSON.stringify(data, null, 2)}</Text>
-            </View>
-        )
-  
-  
+    return (
+        <View style={{ flex: 1, alignItems: 'center' }}>
+            <Text>النتيجة:</Text>
+            <Text>{errorMessage}</Text>
+            <Text>---------------</Text>
+            <Text>{JSON.stringify(data, null, 2)}</Text>
+        </View>
+    )
+
+
   
   
 }
