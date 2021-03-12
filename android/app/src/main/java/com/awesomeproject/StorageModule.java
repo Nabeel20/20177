42e4e94eb425a468ae09aaf48252706dc0978a0b
20-9-20 <nabeelandnizam@gmail.com>
2021-03-12 20:56:27 +0200

    []

--- android/app/src/main/java/com/awesomeproject/StorageModule.java
+++ android/app/src/main/java/com/awesomeproject/StorageModule.java
@@ -46,7 +46,7 @@ public class StorageModule extends ReactContextBaseJavaModule {
   public void get_files_list(Promise promise){
     try {
       File files_path = new File(telegramX_path);
-      String[] files_array = files_path.list();
+      String files_array[] = files_path.list();
       promise.resolve(files_array);
     } catch (Exception e) {
       promise.reject("Something went wrong nabeel", e.getMessage());
