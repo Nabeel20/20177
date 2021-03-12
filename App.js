8e64c9856d4300605c2f5a76ea1746a6bcf93515
20-9-20 <nabeelandnizam@gmail.com>
2021-03-12 17:34:36 +0200

    Update App.js

--- App.js
+++ App.js
@@ -3,16 +3,23 @@ import * as React from 'react';
 import { View, Text, NativeModules } from 'react-native';
 const { Storage } = NativeModules
 function MyComponent() {
-    const [data, setData] = React.useState({build: 40, feeling:'Hope', tryAgain: true});
+    const [data, setData] = React.useState('another try -__-');
+    const [clicked,setClicked] = React.useState('not clicked yet')
     //* we return a a promise form native
     async function get_Files() {
-        let files = await Storage.get_files_list();
-        setData(files)
+        try {
+            let files = await Storage.get_files_list();
+            setData(files);
+        } catch (error) {
+            setClicked('error has happened :' + error)
+        }
+        setClicked('has clicked')
     }
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text onPress={()=> get_Files()}>Get external path</Text>
-            <Text>{JSON.stringify(data, null, 2)}</Text>
+            <Button onPress={() => get_Files()} title="getData" />
+            <Text>{clicked}</Text>
+            <Text>{data}</Text>
         </View>
     )
 }
