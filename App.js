09d067c86d62aa03b863cea5c48ab0a317bb358f
20-9-20 <nabeelandnizam@gmail.com>
2021-03-13 17:40:21 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,71 +1,51 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { View, Text, NativeModules,Button  } from 'react-native';
-const { Storage } = NativeModules
+import { View, Text, NativeModules, FileList } from 'react-native';
+import { FileSystem } from 'react-native-file-access';
+const { Storage } = NativeModules;
 function MyComponent() {
-    const [data, setData] = React.useState('another try -__-');
-    const [ path,setPath] = React.useState('path: click button first');
-    const [clicked, setClicked] = React.useState('not clicked yet');
-    const [visible,setVisible] = React.useState(false);
-    const[fileList,setFileList] = React.useState('No files')
-    async function get_Files() {
-        try {
-            let files = await Storage.get_files_list();
-            if(files.length > 0){
-                setData(files);
-            }else{
-                setData('Nothing here')
-            }
-        } catch (error) {
-            setClicked('error has happened :' + JSON.stringify(error))
-        }
-        setClicked('has clicked')
-    }
-    function get_promise_path(){
-        Storage.get_path().then(value=>{
-            if(value != null || undefined){
-                setPath(`path is: => ${value}`)
-            } 
-        })
-    }
-   async function get_async_path(){
-        let path = await Storage.get_path();
-        if(path != null || undefined){
-            setPath(`path is: ${JSON.stringify(path)}`)
-        } else {
-            setPath('path = null || undefined')
-        }
-    }
-    React.useEffect(()=>{
-        async function get_file_list(){
-            let file = Storage.get_files_list();
-            if(file != null || undefined){
-                setFileList(JSON.stringify(file));
-                setVisible(true)
-            } else {
-                setFileList('file list == null || undefined')
-                setVisible(true)
+    const [data, setData] = React.useState([]);
+    const [loading, setLoading] = React.useState(true);
+    React.useEffect(() => {
+        async function get_database() {
+            let paths = await Storage.get_files_paths();
+            if (paths.length > 0) {
+                for (let i = 0; i < paths.length; i++) {
+                    let file = await FileSystem.readFile(paths[i]);
+                    let file_output = JSON.stringify(file);
+                    file_output.path = paths[i];
+                    setData([...data, file_output]);
+                }
             }
         }
-        get_file_list()
-    },[])
+        get_database();
+        setLoading(false);
+    }, [])
+
+    if (loading) {
+        return (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>جاري التحميل ¯\_( ͡° ͜ʖ ͡°)_/¯</Text>
+            </View>
+        )
+    }
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', padding:5 }}>
-            <Button style={{margin:3}} onPress={() => get_Files()} title="getData" />
-            <Button style={{margin:3}} onPress={() => get_promise_path()} title="getData" />
-            <Button style={{margin:3}} onPress={() => get_async_path()} title="getData" />
-            <Text>{clicked}</Text>
-            <Text>---------</Text>
-            <Text>{data}</Text>
-            <Text>---------</Text>
-            <Text>{path}</Text>
-            <Text>----------</Text>
-            {visible ? <Text>{fileList}</Text>:<Text>UseEffect not working too!</Text>}
+        <View style={{ flex: 1, padding: 10 }}>
+            <FileList
+                data={data}
+                renderItem={({ item }) => {
+                    return (
+                        <View>
+                            <Text>Title: {item.title}</Text>
+                            <Text>Path: {item.path}</Text>
+                            <Text>ــــــــــــــــــــツـــــــــــــــــ</Text>
+                        </View>
+                    )
+                }}
+            />
         </View>
     )
 }
 
-
 export default MyComponent
-
-
