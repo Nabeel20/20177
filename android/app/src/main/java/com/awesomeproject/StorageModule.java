ca7272f94a47dd9f07195dc4eebcfd01442c4a94
20-9-20 <nabeelandnizam@gmail.com>
2021-03-14 10:26:18 +0200

    Update StorageModule.java

--- android/app/src/main/java/com/awesomeproject/StorageModule.java
+++ android/app/src/main/java/com/awesomeproject/StorageModule.java
@@ -10,7 +10,7 @@ import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
 import com.facebook.react.bridge.ReactMethod;
-import com.facebook.recat.bridge.WritableArray;
+import com.facebook.react.bridge.WritableArray;
 
 import java.util.*;
 import java.io.*;
